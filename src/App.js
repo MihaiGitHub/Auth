15c87abc56141ce4f2b99967d1a93f6d8adf572a
Michael S <mihai.sanfran@gmail.com>
2018-12-07 13:35:33 -0700

    Login Form Scaffolding

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native';
 // Should import firebase above all components
 import firebase from 'firebase';
 import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             authDomain: 'authentication-85832.firebaseapp.com',
             databaseURL: 'https://authentication-85832.firebaseio.com',
             projectId: 'authentication-85832',
@@ -19,7 +20,7 @@ class App extends Component {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>My App</Text>
+                <LoginForm />
             </View>
         );
     }
