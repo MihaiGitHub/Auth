5ec8f9a01e1da681d1b3293377ea3939e869c121
Michael S <mihai.sanfran@gmail.com>
2018-12-07 13:20:12 -0700

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+// Should import firebase above all components
 import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            authDomain: 'authentication-85832.firebaseapp.com',
+            databaseURL: 'https://authentication-85832.firebaseio.com',
+            projectId: 'authentication-85832',
+            storageBucket: 'authentication-85832.appspot.com',
+            messagingSenderId: '479477847189'
+        });
+    }
     render() {
         return (
             <View>
