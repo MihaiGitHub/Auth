adf78bf944020c8d6e2c471d6237da70e869494a
Michael S <mihai.sanfran@gmail.com>
2018-12-07 15:28:53 -0700

    Create TextInput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
             <Card>
-                <CardSection />
+                <CardSection>
+                    <TextInput style={{ height: 20, width: 100 }} />
+                </CardSection>
                 <CardSection />
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={() => console.log('eurekae')}>
                         Log In
                     </Button>
                 </CardSection>
